06d9b3379e7ceb4954c75b48fb8f6a801153a360
Sobia Ahmed <sobia_ahmed@outlook.de>
2022-11-23 20:52:22 +0100

    worked on creating functions in Javascript

---
+++ day7/index.js
@@ -0,0 +1,54 @@
+function print(data, data1, data2) {
+	console.log(data, data1, data2)
+}
+
+print(10, true, ["height"])
+
+
+// n + n 
+function addition(number1, number2) {
+	console.log(number1+number2)
+}
+
+addition(10, 10)
+
+
+// 2 * n + 1
+
+function formula(number1) {
+	console.log(2 * number1 + 1)
+}
+
+formula(10)
+
+function areaCircle(number) {
+	console.log(3.16 * (number * number))
+}
+
+areaCircle(5)
+areaCircle(7)
+
+
+//10 / 15
+
+function division(number1, number2) {
+	console.log(number1 / number2)
+
+}
+
+division(10, 5)
+
+
+//2 *1, 3 * 5
+
+function fractionalCalculus(number1, number2, number3, number4) {
+	console.log(number1 * number2 + "/" + number3 * number4)
+}
+
+fractionalCalculus(2, 1, 3, 5)
+
+
+
+
+
+
